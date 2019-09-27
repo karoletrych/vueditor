5ae3d603236760336fe05b7f7a9f48dc2f554b1f
Karol Etrych <karol.etrych@gmail.com>
2019-09-27 19:39:30 +0200

    configure gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const CircularDependencyPlugin = require('circular-dependency-plugin')
 
 module.exports = {
+	publicPath: "vueditor",
     configureWebpack: (config) => ({
         devtool: 'source-map',
         plugins:
